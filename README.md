3a7565dd800b34a2802bd598f04ae9532eeb0396
attributeofextension <leah.carr00@gmail.com>
2024-07-24 16:19:25 +1000

    most of it - print the report - provide the book is created in the same path

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Boot.Dev project
-
-I plan to build a web server, a CLI and a client-side frontend
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def create_end_report_line():
+    return "--- End report ---"
+def create_word_count_line(word_count):
+    return f"{word_count} words found in the document"
+def create_start_report_line(path):
+    return f"--- Begin report of {path} ---"
+def create_line(line):
+    return f"The '{line["char"]}' character was found {line['count']} times"
 
+def sort_on(dict):
+    return dict["count"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def convert_to_list(chars):
+    list = []
+    for key in chars:
+        list.append({ "char": key, "count": chars[key]})
+    list.sort(reverse=True, key=sort_on)
+    return list
+def count_chars(text):
+    chars = {}
+    for char in text:
+        if char not in chars and char.isalpha():
+            chars[char] = 0
+        if char in chars:
+            chars[char] += 1
+    return chars
 
+def count_words(text):
+    word_count = text.split(" ")
+    return len(word_count)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(create_start_report_line("books/frankenstein.txt"))
+        print(create_word_count_line(count_words(file_contents)))
+        print("")
+        for dict in convert_to_list(count_chars(file_contents.lower())):
+            print(create_line(dict))
+        print(create_end_report_line())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
